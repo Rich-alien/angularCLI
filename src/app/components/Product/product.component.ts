c90b3d93f01ce42a9692392f3f02d1e91567fbf3
Richalien <Richalien@yandex.ru>
2021-01-19 16:03:58 +0400

    создан get и set продукта для получение и возврата index товара

--- src/app/components/Product/product.component.ts
+++ src/app/components/Product/product.component.ts
@@ -9,8 +9,19 @@ import {products} from '../../mocks/product.mocks';
 })
 // tslint:disable-next-line:class-name
 export class productComponent {
+  public idProduct: number;
+
+  // tslint:disable-next-line:typedef
   public getProduct() {
     return products;
+  }
+
+  public setIndex(id: number) {
+    this.idProduct = id;
+    console.log(this.idProduct);
+  }
+  public getIndex(){
+    return this.idProduct;
   }
 
 }
