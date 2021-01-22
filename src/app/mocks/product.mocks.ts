47ffe0f651ec9092c63023047943a03adc2131cf
rich-alien <Richalien@yandex.ru>
2021-01-22 09:48:16 +0400

    add for product, count

--- src/app/mocks/product.mocks.ts
+++ src/app/mocks/product.mocks.ts
@@ -2,30 +2,30 @@ import {Product} from '../model/product.model';
 
 export const products: Product[] = [
   {
-    id: 1, name: 'Kayak', description: 'A boat for one person', price: 275
+    id: 1, name: 'Kayak', description: 'A boat for one person', price: 275, count: 1
   },
   {
-    id: 2, name: 'Kayak2', description: 'A boat for one person', price: 500
+    id: 2, name: 'Kayak2', description: 'A boat for one person', price: 500, count: 1
   },
   {
-    id: 3, name: 'Kayak3', description: 'A boat for one person', price: 300
+    id: 3, name: 'Kayak3', description: 'A boat for one person', price: 300, count: 1
   },
   {
-    id: 4, name: 'Kayak4', description: 'A boat for one person', price: 175
+    id: 4, name: 'Kayak4', description: 'A boat for one person', price: 175, count: 1
   },
   {
-    id: 5, name: 'Kayak5', description: 'A boat for one person', price: 875
+    id: 5, name: 'Kayak5', description: 'A boat for one person', price: 875, count: 1
   },
   {
-    id: 6, name: 'Kayak6', description: 'A boat for one person', price: 475
+    id: 6, name: 'Kayak6', description: 'A boat for one person', price: 475, count: 1
   },
   {
-    id: 7, name: 'Kayak7', description: 'A boat for one person', price: 975
+    id: 7, name: 'Kayak7', description: 'A boat for one person', price: 975, count: 1
   },
   {
-    id: 8, name: 'Kayak8', description: 'A boat for one person', price: 1075
+    id: 8, name: 'Kayak8', description: 'A boat for one person', price: 1075, count: 1
   },
   {
-    id: 9, name: 'Kayak9', description: 'A boat for one person', price: 1275
+    id: 9, name: 'Kayak9', description: 'A boat for one person', price: 1275, count: 1
   },
 ];
--- src/app/model/product.model.ts
+++ src/app/model/product.model.ts
@@ -2,5 +2,6 @@ export type Product = {
   'id': number,
   'name': string,
   'description': string,
-  'price': number
+  'price': number,
+  'count': number
 };
