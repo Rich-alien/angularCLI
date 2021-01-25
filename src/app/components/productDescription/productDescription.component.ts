18c7c09f63f247b9c67862123eb4d23e146a1de8
rich-alien <Richalien@yandex.ru>
2021-01-25 16:43:51 +0300

    ts шаблонный

---
+++ src/app/components/productDescription/productDescription.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+  selector: 'app-product-description',
+  templateUrl: './productDescription.component.html',
+  styleUrls: ['./productDescription.component.less'],
+
+})
+
+
+export class ProductDescriptionComponent {
+}
+
