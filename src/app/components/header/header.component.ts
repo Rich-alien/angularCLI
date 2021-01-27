662486f357eeafc3d24a299b50cd148bf4e6b999
rich-alien <Richalien@yandex.ru>
2021-01-27 15:57:58 +0300

    fix

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +7,7 @@ import {Component, Input} from '@angular/core';
 
 })
 export class HeaderComponent  {
-  @Input() authorization;
+  authorization;
   public getUserStatus(status: boolean): void{
     localStorage.setItem('loggedIn', status.toString());
     this.authorization = status;
